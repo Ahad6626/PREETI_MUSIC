aaad8eb9a8f39f2c3c96fd083d77c9b703f1c2f7
Ahad6626 <asad575530@gmail.com>
2025-08-09 11:53:21 +0530

    Update __init__.py

--- ShrutiMusic/utils/__init__.py
+++ ShrutiMusic/utils/__init__.py
@@ -1,25 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
 from .channelplay import *
 from .database import *
 from .decorators import *
@@ -28,4 +6,4 @@ from .formatters import *
 from .inline import *
 from .pastebin import *
 from .sys import *
-from .error import *
+from .imposterdb import *
